d14f8af5f3f168aaa94241556c77bbcde4a5a54b
Nikhilandhale <nandhale17@gmail.com>
2026-06-13 02:47:51 +0530

    Setup Frontend & Backend Foundation

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("SaaS Backend Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
